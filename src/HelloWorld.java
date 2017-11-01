c2e38aee1dc77bfd577920d4b6db46cf89843cd6
tatsuya <tatsuya_hirota@kronos.jp>
2017-11-01 18:40:15 +0900

    update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
 class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		sayHello();
 	}
+
+  public static void sayHello() {
+    System.out.println("Hello World!");
+  }
 }
+

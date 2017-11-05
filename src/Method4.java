f35ccf926ec87e03b4f079c7643baf29ff5767be
Tatsuya Hirota <h1208tatsuya@gmail.com>
2017-11-05 16:32:20 +0900

    update

---
+++ src/Method4.java
@@ -0,0 +1,12 @@
+// Created by hirotatsu on 2017/11/05.
+class Method4 {
+  static void hello(int count) {
+    for (int i = 0; i < count; i++) {
+      System.out.println("hello");
+    }
+  }
+
+  public static void main(String[] args) {
+    hello(3);
+  }
+}

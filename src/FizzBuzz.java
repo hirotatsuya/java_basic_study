de7551d4840b8304f3396305bf83a53d60a76e50
tatsuya <tatsuya_hirota@kronos.jp>
2017-11-14 18:32:40 +0900

    update 修正

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,6 +6,6 @@
  */
 class FizzBuzz {
   public static void main(String[] args) {
-      for (int i = 1; i <= Integer.parseInt(args[0]); i++) System.out.println((i % 15 == 0 ? "FizzBuzz" : i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : i));
+    for (int i = 1; i <= Integer.parseInt(args[0]); i++) System.out.println((i % 15 == 0 ? "FizzBuzz" : i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : i));
   }
 }
